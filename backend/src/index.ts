9acf361313d85992c30995c81a0006bca7cd0997
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-05 10:37:47 +0530

    added jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
@@ -12,13 +14,17 @@ app.post("/api/v1/signup", async (c) => {
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
   const body = await c.req.json();
-  await prisma.user.create({
+ const user= await prisma.user.create({
     data: {
       email: body.email,
       password: body.password,
     },
   });
-  return c.text("Hello Hono!");
+  const token = sign({id: user.id},"secret")
+
+  return c.json({
+    jwt:token
+  });
 });
 
 app.post("/api/v1/signin", (c) => {
